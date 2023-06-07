65f07aa59a426c070e4d617c0cf25bffd5923a1c
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2023-06-07 09:19:04 +0900

    create pictures routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pictures/new'
-  get 'pictures/update'
-  get 'pictures/index'
-  get 'pictures/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pictures do
+      collection do
+          post :confirm
+        end
+    end
 end
